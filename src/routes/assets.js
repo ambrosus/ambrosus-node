be1667bc8b8a7a64a8552ba8b2f67b0409cecf83
ivan <ivanruch@gmail.com>
2018-02-14 15:12:45 +0300

    Fetch asset by id rest call

--- src/routes/assets.js
+++ src/routes/assets.js
@@ -5,13 +5,25 @@ import presignerMiddleware from '../middlewares/presigner_middleware';
 
 import {put} from '../utils/dict_utils';
 
+
+const addLinkToMetadata = (asset, linkHelper) => put(asset, 'metadata.link', linkHelper.linkForAsset(asset.assetId));
+
 export const createAssetHandler = (modelEngine, linkHelper) => async (req, res) => {
   const createdAsset = await modelEngine.createAsset(req.body);
-  const createdAssetWithMetadata = put(createdAsset, 'metadata.link', linkHelper.linkForAsset(createdAsset.assetId));
+  const createdAssetWithMetadata = addLinkToMetadata(createdAsset, linkHelper);
 
   res.status(201)
     .type('json')
     .send(JSON.stringify(createdAssetWithMetadata));
+};
+
+
+export const fetchAssetHandler = (modelEngine, linkHelper) => async (req, res) => {
+  const asset = await modelEngine.getAsset(req.params.assetId);
+  const assetWithMetadata = addLinkToMetadata(asset, linkHelper);
+  res.status(200)
+    .type('json')
+    .send(JSON.stringify(assetWithMetadata));
 };
 
 const assetRouter = (identityManager, modelEngine, linkHelper) => {
@@ -21,6 +33,11 @@ const assetRouter = (identityManager, modelEngine, linkHelper) => {
     bodyParser.json(),
     presignerMiddleware(identityManager),
     asyncMiddleware(createAssetHandler(modelEngine, linkHelper)));
+
+
+  router.get('/:assetId',
+    asyncMiddleware(fetchAssetHandler(modelEngine, linkHelper))
+  );
 
   return router;
 };
