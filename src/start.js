621783469792a89b725c454bb00126accc70d148
Marek Kirejczyk <marek.kirejczyk@gmail.com>
2018-02-14 18:04:18 +0100

    script for creating user

--- src/start.js
+++ src/start.js
@@ -1,27 +1,12 @@
 import Server from './server';
-import {connectToMongo} from './utils/db_utils';
-import {createWeb3} from './utils/web3_tools';
-import IdentityManager from './services/identity_manager';
-import EntityBuilder from './services/entity_builder';
-import EntityStorage from './services/entity_repository';
-import AccountRepository from './services/account_repository';
-import DataModelEngine from './services/data_model_engine';
+import build from './build';
 import LinkHelper from './services/link_helper';
 
 async function start() {
-  const {db} = await connectToMongo();
-  const web3 = await createWeb3();
-
-  const identityManager = new IdentityManager(web3);
-  const entityBuilder = new EntityBuilder(identityManager);
-  const entityRepository = new EntityStorage(db);
-  const accountRepository = new AccountRepository(db);
-  const modelEngine = new DataModelEngine(identityManager, entityBuilder, entityRepository, accountRepository);
+  const modelEngine = build();
   const linkHelper = new LinkHelper();
-
   const server = new Server(modelEngine, linkHelper);
-
-  server.start();  
+  server.start();
 }
 
 start()
---
+++ src/tasks/createAdminUser.js
@@ -0,0 +1,18 @@
+import build from '../build';
+
+async function execute() {
+  const {client, dataModelEngine} = await build();
+  const account = await dataModelEngine.createAccount();
+  console.log('Account created');
+  console.log(`Address: ${account.address}`);
+  console.log(`Secret:  ${account.secret}`);
+  await client.close();
+}
+
+execute()
+  .catch((err) => {
+    console.error(`${err}`);
+    process.exit(1);
+  });
+
+
