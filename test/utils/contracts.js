981a50ec0342e107bb7a6d545ab84acae8d92299
Ivan Rukhavets <ivanruch@gmail.com>
2018-03-05 15:41:53 +0100

    Contract for storing bundle ids (#46)

---
+++ test/utils/contracts.js
@@ -0,0 +1,10 @@
+import BundleRegistryJson from '../../build/contracts/BundleRegistry.json';
+import {deployContract} from '../../src/utils/web3_tools';
+
+
+const deployContracts = async (web3) => {
+  const bundleRegistry = await deployContract(web3, BundleRegistryJson);
+  return {bundleRegistry};
+};
+
+export default deployContracts;
