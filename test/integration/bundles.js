076e4063195da2816eacde325ca67132a34ebb24
Antoni Kędracki <spherefoundry@users.noreply.github.com>
2018-03-08 15:03:22 +0100

    bundle finalisation integration test; (#52)

--- test/integration/bundles.js
+++ test/integration/bundles.js
@@ -1,12 +1,11 @@
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
 import chaiAsPromised from 'chai-as-promised';
-import Apparatus from '../helpers/apparatus';
 import chaiHttp from 'chai-http';
-import {put} from '../../src/utils/dict_utils';
 
-import {createBundle, createAsset} from '../fixtures/assets_events';
-
+import {adminAccountWithSecret} from '../fixtures/account';
+import Apparatus, {apparatusScenarioProcessor} from '../helpers/apparatus';
+import ScenarioBuilder from '../fixtures/scenario_builder';
 
 chai.use(chaiHttp);
 chai.use(sinonChai);
@@ -15,24 +14,70 @@ const {expect} = chai;
 
 describe('Bundles - Integrations', () => {
   let apparatus;
-  const exampleBundleId = '0xabcdef';
-  const exampleEntries = [createAsset(), createAsset()];
-  const exampleBundle = put(createBundle(), {bundleId : exampleBundleId, entries : exampleEntries});
+  let scenario;
+  let res;
+  let entitiesIds;
+
+  const mapEntitiesToIds = (value) => {
+    if (value.assetId) {
+      return value.assetId;
+    } else if (value.eventId) {
+      return value.eventId;
+    }
+
+    throw new Error('unexpected entity type');
+  };
 
   before(async () => {
     apparatus = new Apparatus();
     await apparatus.start();
 
-    await apparatus.entityRepository.storeBundle(exampleBundle);
+    scenario = new ScenarioBuilder(apparatus.identityManager, apparatusScenarioProcessor(apparatus));
+    await scenario.injectAccount(adminAccountWithSecret);
+
+    // this 2 assets and 3 events will go into the bundle
+    entitiesIds = [
+      await scenario.addAsset(0, {timestamp: 0}),
+      await scenario.addAsset(0, {timestamp: 1}),
+      await scenario.addEvent(0, 0, {timestamp: 1}, {someField: 'a'}),
+      await scenario.addEvent(0, 0, {timestamp: 2}, {someField: 'b'}),
+      await scenario.addEvent(0, 1, {timestamp: 2}, {someField: 'c'})
+    ].map(mapEntitiesToIds);
+
+    res = await apparatus.modelEngine.finaliseBundle(1);
+
+    // this additional event should not go into the bundle 
+    await scenario.addEvent(0, 1, {timestamp: 3}, {someField: 'd'});
+  });
+
+  after(async () => {
+    await apparatus.cleanDB();
+    apparatus.stop();
+  });
+
+  describe('finalising bundles', () => {
+    it('should only include assets and events uploaded up until finalisation', () => {
+      const includedEntities = res.content.entries.map(mapEntitiesToIds);
+      expect(includedEntities).to.deep.include.members(entitiesIds);
+      expect(includedEntities.length).to.equal(entitiesIds.length);
+    });
+
+    it('should upload the proof to ethereum, and emit a event', async () => {
+      const emittedEvents = await apparatus
+        .contractManager
+        .bundleProofRegistryContract()
+        .getPastEvents('BundleAdded');
+      const expectedEvent = emittedEvents.filter((value) => value.returnValues.bundleId === res.bundleId);
+      expect(expectedEvent).to.have.length(1);
+    });
   });
 
   describe('getting bundles', () => {
-    it('should get bundle by id', async () => {
+    it('works for existing bundle', async () => {
       const response = await apparatus.request()
-        .get(`/bundle/${exampleBundleId}`);
-      expect(response.body.content).to.deep.equal(exampleBundle.content);
-      expect(response.body.bundleId).to.deep.equal(exampleBundleId);
-      expect(response.body.entries).to.deep.equal(exampleEntries);
+        .get(`/bundle/${res.bundleId}`);
+      expect(response.body.content).to.deep.equal(res.content);
+      expect(response.body.bundleId).to.equal(res.bundleId);
     });
 
     it('return 404 if bundle with requested id does not exist', async () => {
@@ -41,10 +86,5 @@ describe('Bundles - Integrations', () => {
       await expect(request).to.eventually.be.rejected
         .and.have.property('status', 404);
     });
-  });
-
-  after(async () => {
-    await apparatus.cleanDB();
-    apparatus.stop();
   });
 });
