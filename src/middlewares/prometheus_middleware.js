8151b1efe70d3c8173a99dedbde2e2fb4988dfda
Dani Hodovic <danihodovic@users.noreply.github.com>
2018-07-05 19:43:53 +0700

    Expose Prometheus metrics (#162)
    Exposes Prometheus metrics on the /metrics endpoint.
    
    The prometheus client collects default metrics on Node.js such as heap
    and cpu usage.
    
    For a detailed list of default metrics see:
    https://github.com/siimon/prom-client/tree/master/lib/metrics
    
    Additionally I've added http request duration metrics which should give
    us:
    
    - mean request duration
    - median request duration
    - request duration quantiles
    - number of HTTP requests per second
    
    We can now also alert when ambnode is down by using the Prometheus
    metric UP.

---
+++ src/middlewares/prometheus_middleware.js
@@ -0,0 +1,36 @@
+/*
+Copyright: Ambrosus Technologies GmbH
+Email: tech@ambrosus.com
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+const excludedPaths = ['/health', '/metrics'];
+
+const prometheusMiddleware = (promClient, register) => {
+  const httpRequestDurationSeconds = new promClient.Histogram({
+    register: [register],
+    name: 'http_request_duration_seconds',
+    help: 'Request duration in seconds',
+    buckets: promClient.linearBuckets(0.1, 0.2, 20),
+    labelNames: ['path', 'status']
+  });
+
+  return (req, res, next) => {
+    if (!excludedPaths.includes(req.path)) {
+      // Save req.path in a local variable because it changed during
+      // `res.on('finish')` to `/`.
+      const {path} = req;
+      const endTimer = httpRequestDurationSeconds.startTimer();
+
+      res.on('finish', () => endTimer({
+        path,
+        status: res.statusCode
+      }));
+    }
+    next();
+  };
+};
+
+export default prometheusMiddleware;
---
+++ test/integration/prometheus_metrics.js
@@ -0,0 +1,44 @@
+/*
+Copyright: Ambrosus Technologies GmbH
+Email: tech@ambrosus.com
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+import chai from 'chai';
+import Apparatus from '../helpers/apparatus';
+
+const {expect} = chai;
+
+describe('Prometheus middleware tests', () => {
+  let apparatus;
+
+  beforeEach(async () => {
+    apparatus = new Apparatus();
+    await apparatus.start();
+  });
+
+  afterEach(async () => {
+    await apparatus.stop();
+  });
+
+  it('metrics endpoint', async () => {
+    // Trigger a request which will add an entry of our request duration
+    // histogram. /metrics and /health are excluded.
+    await apparatus.request().get('/nodeInfo');
+
+    const response = await apparatus.request().get('/metrics');
+    expect(response.status).to.eql(200);
+
+    // Built in library metrics
+    expect(response.text.indexOf('process_cpu_user_seconds_total') > -1)
+      .to.eql(true);
+
+    // Custom metrics
+    expect(response.text.indexOf(
+      'http_request_duration_seconds_count{path="/nodeInfo",status="200"} 1'
+    ) > -1)
+      .to.eql(true);
+  });
+});
