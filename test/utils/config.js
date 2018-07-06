54562564ba80eabfc47b31ff0dc7bb2a7f89654b
Ivan Rukhavets <ivanruch@gmail.com>
2018-07-06 16:58:25 +0200

    Spelling (#169)

--- test/utils/config.js
+++ test/utils/config.js
@@ -13,17 +13,17 @@ import Config from '../../src/utils/config';
 const {expect} = chai;
 
 describe('Config', () => {
-  it('intialisation', () => {
+  it('initialisation', () => {
     const config = new Config({attribute: 2});
     expect(config.attributes).to.deep.eq({attribute: 2});
   });
 
-  it('withAttibutes (initially empty)', () => {
+  it('withAttributes (initially empty)', () => {
     const config = new Config({}).withAttributes({attribute: 1});
     expect(config.attributes).to.deep.eq({attribute: 1});
   });
 
-  it('withAttibutes (override)', () => {
+  it('withAttributes (override)', () => {
     const config = new Config({attribute: 2}).withAttributes({attribute: 1});
     expect(config.attributes).to.deep.eq({attribute: 1});
   });
