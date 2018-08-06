3b4cdf7c619a62970a0cd174d9dfea45dd1d4a9f
Bartłomiej Rutkowski <bar.rutkow@gmail.com>
2018-08-06 13:53:26 +0200

    Hermes upload worker added (#195)

--- config/config.js
+++ config/config.js
@@ -17,6 +17,7 @@ module.exports = Object.freeze({
 
   headContractAddress: process.env.HEAD_CONTRACT_ADDRESS,
   bundleSizeLimit: parseInt(process.env.BUNDLE_SIZE_LIMIT, 10) || 10000,
+  storagePeriods: parseInt(process.env.STORAGE_PERIODS, 10) || 1,
 
   maximumEntityTimestampOvertake:
     parseInt(process.env.MAXIMUM_ENTITY_TIMESTAMP_OVERTAKE, 10) || 86400,
