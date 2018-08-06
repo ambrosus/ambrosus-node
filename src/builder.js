a7215854c180dc4ac7634e6d7b87546d8b73243f
Ivan Rukhavets <ivanruch@gmail.com>
2018-08-06 11:53:35 +0200

    Disable routes (#193)

--- src/builder.js
+++ src/builder.js
@@ -22,7 +22,7 @@ import {connectToMongo} from './utils/db_utils';
 import HttpsClient from './utils/https_client';
 import TokenAuthenticator from './utils/token_authenticator';
 import {createWeb3, getDefaultAddress} from './utils/web3_tools';
-import RolesRepository from './services/roles_repository';
+import RolesRepository, {Role} from './services/roles_repository';
 
 class Builder {
   async ensureAdminAccountExist() {
@@ -31,12 +31,13 @@ class Builder {
 
   async ensureAccountIsOnboarded(allowedRoles) {
     const role = await this.rolesRepository.onboardedRole(getDefaultAddress(this.web3));
-    if (role.name === 'NONE') {
+    if (role.is(Role.NONE)) {
       throw new Error('You must be onboarded in order to start a node');
     }
-    if (!allowedRoles.includes(role.name)) {
+    if (!allowedRoles.some((allowedRole) => role.is(allowedRole))) {
       throw new Error(`You must be onboarded as one of the following roles: ${allowedRoles.toString()}. Instead onboarded as ${role.name}`);
     }
+    return role;
   }
 
   async build(config, dependencies = {}) {
