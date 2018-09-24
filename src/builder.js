18acdf121f7233c5cd65b3c7e67b6edf6f2c7d1e
Bartłomiej Rutkowski <bar.rutkow@gmail.com>
2018-09-24 14:23:27 +0200

    updated wrappers interfaces

--- src/builder.js
+++ src/builder.js
@@ -53,7 +53,10 @@ class Builder {
     const {headContractAddress} = this.config;
     this.contractManager = new ContractManager(this.web3, headContractAddress);
     this.rolesRepository = new RolesRepository(this.contractManager.rolesWrapper, this.contractManager.configWrapper);
+    this.identityManager = new IdentityManager(this.web3);
     this.uploadRepository = new UploadRepository(
+      this.web3,
+      this.identityManager,
       this.contractManager.uploadsWrapper,
       this.contractManager.shelteringWrapper,
       this.contractManager.rolesWrapper,
@@ -61,7 +64,6 @@ class Builder {
       this.contractManager.configWrapper
     );
     this.challengesRepository = new ChallengesRepository(this.contractManager.challengesWrapper, this.contractManager.configWrapper);
-    this.identityManager = new IdentityManager(this.web3);
     this.tokenAuthenticator = new TokenAuthenticator(this.identityManager);
     const {maximumEntityTimestampOvertake} = this.config;
     this.entityBuilder = new EntityBuilder(this.identityManager, maximumEntityTimestampOvertake);
