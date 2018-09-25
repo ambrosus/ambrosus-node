a1fe736d100e400f2282d8e8dc39c02780b88e76
Bartłomiej Rutkowski <bartek@ethworks.io>
2018-09-25 11:39:47 +0200

    another fix

--- src/utils/deployment.js
+++ src/utils/deployment.js
@@ -41,47 +41,4 @@ const deployAll = async (web3, privateKey, logger) => {
   return head;
 };
 
-// const deployLibraries = async (web3) => ({SafeMathExtensions: await deployContract(web3, contractJsons.safeMathExtensions.abi, contractJsons.safeMathExtensions.bytecode)});
-
-// const getContractConstructor = (contractJson) => contractJson.abi.find((value) => value.type === 'constructor');
-
-// const contractToAddress = (contract) => contract.options.address;
-
-// const linkLibraries = (libraries, contracts) => {
-//   for (const contractName in contracts) {
-//     for (const lib of Object.entries(libraries)) {
-//       const [libName, libContract] = lib;
-//       link(contracts[contractName], libName, libContract);
-//     }
-//   }
-// };
-
-// const deployOne = async (defaultJson, web3, head) => {
-//   const contractJson = defaultJson;
-
-//   const constructorArgs = [];
-//   const constructor = getContractConstructor(contractJson);
-//   if (constructor !== undefined && constructor.inputs.find((input) => input.name === '_head' && input.type === 'address') !== undefined) {
-//     constructorArgs.push(head.options.address);
-//   }
-
-//   return deployContract(web3, contractJson.abi, contractJson.bytecode, constructorArgs);
-// };
-
-// const deployContracts = async (contractJsons, web3, head) => {
-//   const deployedContracts = {};
-//   for (const contractName in contractJsons) {
-//     deployedContracts[contractName] = await deployOne(contractJsons[contractName], web3, head);
-//   }
-//   const contextConstructorParams = getContractConstructor(contractJsons.context)
-//     .inputs
-//     .map((input) => input.name.slice(1));
-
-//   if (!contextConstructorParams.every((key) => contractJsons[key] !== undefined)) {
-//     throw 'Missing a parameter for context constructor';
-//   }
-
-//   return contextConstructorParams.map((key) => contractToAddress(deployedContracts[key]));
-// };
-
 export default deployAll;
