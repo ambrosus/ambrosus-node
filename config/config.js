342227bc5c63c5ccbc02f848862e2efb5aa7593d
Conor Seabrook <conor.seabrook@gmail.com>
2018-11-13 13:15:52 -0500

    insufficient funds exception and low balance warnings sent to sentry

--- config/config.js
+++ config/config.js
@@ -26,6 +26,8 @@ module.exports = Object.freeze({
   mongoUser: process.env.MONGO_USER,
   mongoPassword: process.env.MONGO_PASSWORD,
 
+  lowFundsWarningAmount: process.env.LOW_FUNDS_WARNING || 1000,
+
   headContractAddress: process.env.HEAD_CONTRACT_ADDRESS,
   challengeResolutionStrategy: process.env.CHALLENGE_RESOLUTION_STRATEGY || 'resolve_all_strategy',
   uploadStrategy: process.env.UPLOAD_STRATEGY || 'regular_interval_upload_strategy',
