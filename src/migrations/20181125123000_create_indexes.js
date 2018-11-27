dd41197bc2ed2497bc0972c2d121d1c82c400857
ivan <ivanruch@gmail.com>
2018-11-27 12:07:32 +0100

    Try to fix CI

--- src/migrations/20181125123000_create_indexes.js
+++ src/migrations/20181125123000_create_indexes.js
@@ -10,31 +10,31 @@ This Source Code Form is “Incompatible With Secondary Licenses”, as defined 
 // eslint-disable-next-line import/prefer-default-export
 export const up = async (db, config, logger) => {
   // assets
-  await db.collection('assets').createIndex({assetId: 1});
-  await db.collection('assets').createIndex({'content.idData.timestamp': 1});
-  await db.collection('assets').createIndex({'content.idData.createdBy': 1});
+  await db.collection('assets').ensureIndex({assetId: 1});
+  await db.collection('assets').ensureIndex({'content.idData.timestamp': 1});
+  await db.collection('assets').ensureIndex({'content.idData.createdBy': 1});
 
   // events
-  await db.collection('events').createIndex({eventId: 1});
-  await db.collection('events').createIndex({'content.idData.timestamp': 1});
-  await db.collection('events').createIndex({'content.idData.createdBy': 1});
-  await db.collection('events').createIndex({'content.idData.accessLevel': 1});
-  await db.collection('events').createIndex({'content.idData.assetId': 1});
-  await db.collection('events').createIndex({'content.data.type': 1});
-  await db.collection('events').createIndex({'content.data.geoJson': '2dsphere'});
+  await db.collection('events').ensureIndex({eventId: 1});
+  await db.collection('events').ensureIndex({'content.idData.timestamp': 1});
+  await db.collection('events').ensureIndex({'content.idData.createdBy': 1});
+  await db.collection('events').ensureIndex({'content.idData.accessLevel': 1});
+  await db.collection('events').ensureIndex({'content.idData.assetId': 1});
+  await db.collection('events').ensureIndex({'content.data.type': 1});
+  await db.collection('events').ensureIndex({'content.data.geoJson': '2dsphere'});
 
   // bundles
-  await db.collection('bundles').createIndex({bundleId: 1});
-  await db.collection('bundles').createIndex({'content.idData.timestamp': 1});
-  await db.collection('bundles').createIndex({'content.idData.createdBy': 1});
+  await db.collection('bundles').ensureIndex({bundleId: 1});
+  await db.collection('bundles').ensureIndex({'content.idData.timestamp': 1});
+  await db.collection('bundles').ensureIndex({'content.idData.createdBy': 1});
 
   // for internal use by repository
-  await db.collection('assets').createIndex({'repository.bundleStubId': 1});
-  await db.collection('events').createIndex({'repository.bundleStubId': 1});
+  await db.collection('assets').ensureIndex({'repository.bundleStubId': 1});
+  await db.collection('events').ensureIndex({'repository.bundleStubId': 1});
 
   // worker metadata collections
-  await db.collection('workerLogs').createIndex({timestamp: -1});
-  await db.collection('workerTasks').createIndex({workType : 1}, {unique: true});
+  await db.collection('workerLogs').ensureIndex({timestamp: -1});
+  await db.collection('workerTasks').ensureIndex({workType : 1}, {unique: true});
 
   logger.info(`Created indexes`);
 };
