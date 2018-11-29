cafd40f88e02392dce4dff78959d885c1523da13
ivan <ivanruch@gmail.com>
2018-11-29 12:25:30 +0100

    Added worker tasks TTL

---
+++ src/migrations/20181129100000_worker_task_ttl.js
@@ -0,0 +1,15 @@
+/*
+Copyright: Ambrosus Technologies GmbH
+Email: tech@ambrosus.com
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+
+// eslint-disable-next-line import/prefer-default-export
+export const up = async (db, {workerTasksTTLInSeconds}, logger) => {
+  await db.collection('workerTasks').dropIndexes();
+  await db.collection('workerTasks').createIndex({startTime: -1}, {expireAfterSeconds: workerTasksTTLInSeconds});
+  logger.info(`Added index to worker tasks with TTL=${workerTasksTTLInSeconds}`);
+};
