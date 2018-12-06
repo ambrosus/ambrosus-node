01bb9a988453f67bbcd7936094c18c3769a949be
Ivan Rukhavets <ivanruch@gmail.com>
2018-12-06 15:46:39 +0100

    Deduplicate bundles in migrations (#302)

--- src/migrations/20181206153000_move_out_bundles_metadata.js
+++ src/migrations/20181206153000_move_out_bundles_metadata.js
@@ -7,8 +7,34 @@ This Source Code Form is subject to the terms of the Mozilla Public License, v. 
 This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
 */
 
+const deduplicateBundles = async (db) => {
+  const cursor = await db.collection('bundles').aggregate(
+    [{
+      $group: {
+        _id: '$bundleId',
+        dups: {$addToSet: '$_id'},
+        count: {$sum: 1}
+      }
+    },
+    {
+      $match:
+        {
+          count: {$gt: 1}
+        }
+    }]
+  );
+  while (await cursor.hasNext()) {
+    const document = await cursor.next();
+    const toRemove = document.dups.slice(1);
+    await db.collection('bundles').deleteMany({
+      _id: {$in: toRemove}
+    });
+  }
+};
+
 // eslint-disable-next-line import/prefer-default-export
 export const up = async (db, config, logger) => {
+  await deduplicateBundles(db);
   await db.collection('bundle_metadata').createIndex({bundleId : 1}, {unique: true});
 
   await db.collection('bundles').dropIndex('bundleId_1');
