6038ee8bde582d52b141b6e957d9020872c7224c
Dani Hodovic <danihodovic@users.noreply.github.com>
2018-12-18 13:09:20 +0700

    Add process instrumentation (#324)
    I've seen unhandledRejection errors bubble up to the process when
    operating Hermes workers in production. This causes a zombie process
    which isn't operational, but continues running.
    
    The commit introduces the following changes:
    
    on unhandledRejection or uncaughtException errors that are not handled
    and bubble up to the process, do the following:
    
    - exit with a status code of 1 so that the process is restarted by it's
      supervisor in a fresh state (in our case by the Docker daemon)
    
    - log the error
    
    - report the error to Sentry (if Sentry is enabled)
    
    Additionally we enable Sentry for the Hermes and Atlas workers rather
    than having it run only on the server.

---
+++ test/utils/instrument_process.js
@@ -0,0 +1,89 @@
+/*
+Copyright: Ambrosus Technologies GmbH
+Email: tech@ambrosus.com
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+import chai from 'chai';
+import * as sinon from 'sinon';
+import {instrumentProcess} from '../../src/utils/instrument_process';
+import config from '../../config/config';
+
+const {expect} = chai;
+
+describe('#instrumentProcess', () => {
+  let sandbox;
+  let startFn;
+  let sentry;
+  let sentryClose;
+  let logger;
+  let unhandledRejectionListeners;
+  let uncaughtExceptionListeners;
+
+  beforeEach(() => {
+    unhandledRejectionListeners = process.listeners('unhandledRejection');
+    uncaughtExceptionListeners = process.listeners('uncaughtException');
+    process.removeAllListeners('unhandledRejection');
+    process.removeAllListeners('uncaughtException');
+
+    sandbox = sinon.createSandbox();
+    startFn = sandbox.spy();
+    sentryClose = sandbox.stub().returns(Promise.resolve());
+    sentry = {
+      init: sandbox.spy(),
+      captureException: sandbox.spy(),
+      getCurrentHub: () => ({
+        getClient: () => ({
+          close: sentryClose
+        })
+      })
+    };
+    logger = {
+      error: sandbox.spy()
+    };
+    sandbox.stub(process, 'exit');
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+    unhandledRejectionListeners.forEach((listener) => {
+      process.on('unhandledRejection', listener);
+    });
+    uncaughtExceptionListeners.forEach((listener) => {
+      process.on('uncaughtException', listener);
+    });
+  });
+
+  it('initiates the Sentry client', () => {
+    instrumentProcess(startFn, sentry, logger);
+    expect(sentry.init).to.have.been.calledOnceWith({
+      dsn: config.sentryDSN,
+      environment: process.env.NODE_ENV,
+      ignoreErrors: []
+    });
+  });
+
+  it('calls the startFn', () => {
+    instrumentProcess(startFn, sentry, logger);
+    expect(startFn).to.have.been.calledOnceWith(logger);
+  });
+
+  it('on unhanledRejection reports errors', () => {
+    const err = new Error('hodovic');
+    const startFn = () => {
+      Promise.reject(err);
+    };
+    instrumentProcess(startFn, sentry, logger);
+    // wait after the pending events have been processed
+    setImmediate(() => {
+      expect(logger.error).to.have.been.calledOnceWith({
+        message: 'unhandledRejection bubbled up to the process',
+        error: err
+      });
+      expect(sentry.captureException).to.have.been.calledOnceWith(err);
+      expect(sentryClose).to.have.been.calledOnceWith(2000);
+    });
+  });
+});
