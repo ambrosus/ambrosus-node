12788bb4aebe672fd85d082b00608ebc204d2b22
ivan <ivanruch@gmail.com>
2019-05-21 11:21:41 +0200

    Added migration

---
+++ src/migrations/20190521104500_add_hold_until_index.js
@@ -0,0 +1,15 @@
+/*
+Copyright: Ambrosus Inc.
+Email: tech@ambrosus.com
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+
+// eslint-disable-next-line import/prefer-default-export
+export const up = async (db, config, logger) => {
+  await db.collection('bundle_metadata').createIndex({'repository.holdUntil': -1});
+
+  logger.info(`Added index to bundle_metadata.repository.holdUntil`);
+};
