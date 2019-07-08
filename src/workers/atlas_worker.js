aed462d5357a6630186fc08e69ea816a3d606843
Andrii Rozinko <a.rozinko@minerall.io>
2019-07-08 21:30:33 +0300

    Now the check for the possibility of resolving is before downloading the bundle.

--- src/workers/atlas_worker.js
+++ src/workers/atlas_worker.js
@@ -98,16 +98,17 @@ export default class AtlasWorker extends PeriodicWorker {
         await this.addLog('Decided not to download bundle', challenge);
         return false;
       }
-      const bundleMetadata = await this.tryToDownload(challenge);
-      if (!await this.strategy.shouldResolveChallenge(bundleMetadata)) {
-        this.atlasChallengeMetrics.inc({status: atlasChallengeStatus.shouldNotResolve});
-        await this.addLog('Challenge resolution cancelled', challenge);
-        return false;
-      }
 
       if (!await this.isTurnToResolve(challenge)) {
         this.atlasChallengeMetrics.inc({status: atlasChallengeStatus.shouldNotResolve});
         await this.addLog(`Not the node's turn to resolve`, challenge);
+        return false;
+      }
+
+      const bundleMetadata = await this.tryToDownload(challenge);
+      if (!await this.strategy.shouldResolveChallenge(bundleMetadata)) {
+        this.atlasChallengeMetrics.inc({status: atlasChallengeStatus.shouldNotResolve});
+        await this.addLog('Challenge resolution cancelled', challenge);
         return false;
       }
 
