920325a56275c6471998df10bafb3406dc4e0241
Andrii Rozinko <a.rozinko@minerall.io>
2019-07-29 16:25:15 +0300

    Appling the improved superclass to challenge and transfer resolvers.

--- src/workers/atlas_resolvers/atlas_challenge_resolver.js
+++ src/workers/atlas_resolvers/atlas_challenge_resolver.js
@@ -8,11 +8,10 @@ This Source Code Form is “Incompatible With Secondary Licenses”, as defined 
 */
 
 import promClient from 'prom-client';
-import AtlasParticipationStrategy from '../atlas_strategies/atlas_participation_strategy';
-import {AtlasResolver, atlasResolutionStatus} from './atlas_resolver';
-import {getDefaultAddress} from '../../utils/web3_tools';
+import BundleShelteringResolver from './bundle_sheltering_resolver';
+import {atlasResolutionStatus} from './atlas_resolver';
 
-export default class AtlasChallengeResolver extends AtlasResolver {
+export default class AtlasChallengeResolver extends BundleShelteringResolver {
   constructor(
     web3,
     dataModelEngine,
@@ -22,116 +21,31 @@ export default class AtlasChallengeResolver extends AtlasResolver {
     workerLogRepository,
     logger
   ) {
-    super();
-    this.web3 = web3;
-    this.dataModelEngine = dataModelEngine;
-    this.strategy = strategy;
-    this.challengesRepository = challengesRepository;
-    this.failedChallengesCache = failedChallengesCache;
-    this.workerLogRepository = workerLogRepository;
-    this.logger = logger;
-
-    if (!(this.strategy instanceof AtlasParticipationStrategy)) {
-      throw new Error('A valid challenge strategy must be provided');
-    }
+    super(web3,
+      dataModelEngine,
+      challengesRepository,
+      failedChallengesCache,
+      strategy,
+      workerLogRepository,
+      logger,
+      'Challenge'
+    );
   }
 
-  async tryToResolve({bundleId}, {challengeId}) {
-    await this.challengesRepository.resolveChallenge(challengeId);
-    await this.dataModelEngine.markBundleAsSheltered(bundleId);
-    await this.addLog('🍾 Yahoo! The bundle is ours.', {bundleId});
+  getPropositionId(challenge) {
+    return challenge.challengeId;
   }
 
-  async tryToDownload({sheltererId, bundleId, challengeId}) {
-    const challengeExpirationTime = await this.challengesRepository.getChallengeExpirationTimeInMs(challengeId);
-    const metadata = await this.dataModelEngine.downloadBundle(bundleId, sheltererId, challengeExpirationTime);
-    await this.addLog(`Bundle fetched`, {sheltererId, bundleId, challengeId});
-    return metadata;
-  }
-
-  async isTurnToResolve({challengeId}) {
-    const currentResolver = await this.challengesRepository.getChallengeDesignatedShelterer(challengeId);
-    return (currentResolver === getDefaultAddress(this.web3));
-  }
-
-  async tryWithChallenge(challenge) {
-    try {
-      if (this.failedChallengesCache.didResolutionFailRecently(challenge.challengeId)) {
-        return false;
-      }
-      if (!await this.isTurnToResolve(challenge)) {
-        this.atlasChallengeMetrics.inc({status: atlasResolutionStatus.shouldNotResolve});
-        await this.addLog(`Not the node's turn to resolve`, challenge);
-        return false;
-      }
-
-      if (!await this.strategy.shouldFetchBundle(challenge)) {
-        this.atlasChallengeMetrics.inc({status: atlasResolutionStatus.shouldNotFetch});
-        await this.addLog('Decided not to download bundle', challenge);
-        return false;
-      }
-
-      const bundleMetadata = await this.tryToDownload(challenge);
-      if (!await this.strategy.shouldResolve(bundleMetadata)) {
-        this.atlasChallengeMetrics.inc({status: atlasResolutionStatus.shouldNotResolve});
-        await this.addLog('Challenge resolution cancelled', challenge);
-        return false;
-      }
-
-      await this.tryToResolve(bundleMetadata, challenge);
-      await this.strategy.afterResolution(challenge);
-      this.atlasChallengeMetrics.inc({status: atlasResolutionStatus.resolved});
-      return true;
-    } catch (err) {
-      this.failedChallengesCache.rememberFailedResolution(challenge.challengeId, this.strategy.retryTimeout);
-      await this.addLog(`Failed to resolve challenge: ${err.message || err}`, challenge, err.stack);
-      this.atlasChallengeMetrics.inc({status: atlasResolutionStatus.failed});
-      return false;
-    }
-  }
-
-  async addLog(message, additionalFields, stacktrace) {
-    const log = {
-      message,
-      ...additionalFields
-    };
-    this.logger.info({...log, stacktrace});
-    await this.workerLogRepository.storeLog({timestamp: new Date(), ...log});
+  getSheltererId(challenge) {
+    return challenge.sheltererId;
   }
 
   addMetrics(registry) {
-    this.atlasChallengeMetrics = new promClient.Counter({
+    this.atlasResolverMetrics = new promClient.Counter({
       name: 'atlas_challenges_total',
       help: `Total number of challenges. Status label is one of [${Object.values(atlasResolutionStatus)}]`,
       labelNames: ['status'],
       registers: [registry]
     });
-  }
-
-  async resolve(entity) {
-    return await this.tryWithChallenge(entity);
-  }
-
-  async resolveOne() {
-    const challenges = await this.challengesRepository.ongoingResolutions();
-    const recentlyFailedChallenges = challenges.filter(({challengeId}) => challengeId in this.failedChallengesCache.failedResolutionsEndTime);
-    await this.addLog(`Challenges preselected for resolution: ${challenges.length} (out of which ${recentlyFailedChallenges.length} have failed recently)`);
-    for (const challenge of challenges) {
-      const successful = await this.tryWithChallenge(challenge);
-      if (successful) {
-        break;
-      }
-    }
-    this.failedChallengesCache.clearOutdatedResolutions();
-  }
-
-  async resolveAll() {
-    const challenges = await this.challengesRepository.ongoingResolutions();
-    const recentlyFailedChallenges = challenges.filter(({challengeId}) => challengeId in this.failedChallengesCache.failedResolutionsEndTime);
-    await this.addLog(`Challenges preselected for resolution: ${challenges.length} (out of which ${recentlyFailedChallenges.length} have failed recently)`);
-    for (const challenge of challenges) {
-      await this.tryWithChallenge(challenge);
-    }
-    this.failedChallengesCache.clearOutdatedResolutions();
   }
 }
--- src/workers/atlas_resolvers/atlas_transfer_resolver.js
+++ src/workers/atlas_resolvers/atlas_transfer_resolver.js
@@ -8,11 +8,10 @@ This Source Code Form is “Incompatible With Secondary Licenses”, as defined 
 */
 
 import promClient from 'prom-client';
-import AtlasParticipationStrategy from '../atlas_strategies/atlas_participation_strategy';
-import {AtlasResolver, atlasResolutionStatus} from './atlas_resolver';
-import {getDefaultAddress} from '../../utils/web3_tools';
+import BundleShelteringResolver from './bundle_sheltering_resolver';
+import {atlasResolutionStatus} from './atlas_resolver';
 
-export default class AtlaTransferResolver extends AtlasResolver {
+export default class AtlaTransferResolver extends BundleShelteringResolver {
   constructor(
     web3,
     dataModelEngine,
@@ -22,116 +21,31 @@ export default class AtlaTransferResolver extends AtlasResolver {
     workerLogRepository,
     logger
   ) {
-    super();
-    this.web3 = web3;
-    this.dataModelEngine = dataModelEngine;
-    this.strategy = strategy;
-    this.transfersRepository = transfersRepository;
-    this.failedTransfersCache = failedTransfersCache;
-    this.workerLogRepository = workerLogRepository;
-    this.logger = logger;
-
-    if (!(this.strategy instanceof AtlasParticipationStrategy)) {
-      throw new Error('A valid transfer strategy must be provided');
-    }
+    super(web3,
+      dataModelEngine,
+      transfersRepository,
+      failedTransfersCache,
+      strategy,
+      workerLogRepository,
+      logger,
+      'Transfer'
+    );
   }
 
-  async tryToResolve({bundleId}, {transferId}) {
-    await this.transfersRepository.resolveTransfer(transferId);
-    await this.dataModelEngine.markBundleAsSheltered(bundleId);
-    await this.addLog('🍾 Yahoo! The bundle is ours.', {bundleId});
+  getPropositionId(transfer) {
+    return transfer.transferId;
   }
 
-  async tryToDownload({donorId, bundleId, transferId}) {
-    const transferExpirationTime = await this.transfersRepository.getTransferExpirationTimeInMs(transferId);
-    const metadata = await this.dataModelEngine.downloadBundle(bundleId, donorId, transferExpirationTime);
-    await this.addLog(`Bundle fetched`, {donorId, bundleId, transferId});
-    return metadata;
-  }
-
-  async isTurnToResolve({transferId}) {
-    const currentResolver = await this.transfersRepository.getTransferDesignatedShelterer(transferId);
-    return (currentResolver === getDefaultAddress(this.web3));
-  }
-
-  async tryWithTransfer(transfer) {
-    try {
-      if (this.failedTransfersCache.didResolutionFailRecently(transfer.transferId)) {
-        return false;
-      }
-      if (!await this.isTurnToResolve(transfer)) {
-        this.atlasTransferMetrics.inc({status: atlasResolutionStatus.shouldNotResolve});
-        await this.addLog(`Not the node's turn to resolve`, transfer);
-        return false;
-      }
-
-      if (!await this.strategy.shouldFetchBundle(transfer)) {
-        this.atlasTransferMetrics.inc({status: atlasResolutionStatus.shouldNotFetch});
-        await this.addLog('Decided not to download bundle', transfer);
-        return false;
-      }
-
-      const bundleMetadata = await this.tryToDownload(transfer);
-      if (!await this.strategy.shouldResolve(bundleMetadata)) {
-        this.atlasTransferMetrics.inc({status: atlasResolutionStatus.shouldNotResolve});
-        await this.addLog('Transfer resolution cancelled', transfer);
-        return false;
-      }
-
-      await this.tryToResolve(bundleMetadata, transfer);
-      await this.strategy.afterResolution(transfer);
-      this.atlasTransferMetrics.inc({status: atlasResolutionStatus.resolved});
-      return true;
-    } catch (err) {
-      this.failedTransfersCache.rememberFailedResolution(transfer.transferId, this.strategy.retryTimeout);
-      await this.addLog(`Failed to resolve transfer: ${err.message || err}`, transfer, err.stack);
-      this.atlasTransferMetrics.inc({status: atlasResolutionStatus.failed});
-      return false;
-    }
-  }
-
-  async addLog(message, additionalFields, stacktrace) {
-    const log = {
-      message,
-      ...additionalFields
-    };
-    this.logger.info({...log, stacktrace});
-    await this.workerLogRepository.storeLog({timestamp: new Date(), ...log});
+  getSheltererId(transfer) {
+    return transfer.donorId;
   }
 
   addMetrics(registry) {
-    this.atlasTransferMetrics = new promClient.Counter({
+    this.atlasResolverMetrics = new promClient.Counter({
       name: 'atlas_transfers_total',
       help: `Total number of transfers. Status label is one of [${Object.values(atlasResolutionStatus)}]`,
       labelNames: ['status'],
       registers: [registry]
     });
-  }
-
-  async resolve(entity) {
-    return await this.tryWithTransfer(entity);
-  }
-
-  async resolveOne() {
-    const transfers = await this.transfersRepository.ongoingResolutions();
-    const recentlyFailedTransfers = transfers.filter(({transferId}) => transferId in this.failedTransfersCache.failedResolutionsEndTime);
-    await this.addLog(`Transfers preselected for resolution: ${transfers.length} (out of which ${recentlyFailedTransfers.length} have failed recently)`);
-    for (const transfer of transfers) {
-      const successful = await this.tryWithTransfer(transfer);
-      if (successful) {
-        break;
-      }
-    }
-    this.failedTransfersCache.clearOutdatedResolutions();
-  }
-
-  async resolveAll() {
-    const transfers = await this.transfersRepository.ongoingResolutions();
-    const recentlyFailedTransfers = transfers.filter(({transferId}) => transferId in this.failedTransfersCache.failedResolutionsEndTime);
-    await this.addLog(`Transfers preselected for resolution: ${transfers.length} (out of which ${recentlyFailedTransfers.length} have failed recently)`);
-    for (const transfer of transfers) {
-      await this.tryWithTransfer(transfer);
-    }
-    this.failedTransfersCache.clearOutdatedResolutions();
   }
 }
