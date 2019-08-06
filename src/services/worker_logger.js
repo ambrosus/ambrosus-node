6a4668a0f98c4b11909270a3708644c57db014eb
Andrii Rozinko <a.rozinko@minerall.io>
2019-08-06 09:14:55 +0300

    Added WorkerLogger with addLog functionality.

---
+++ src/services/worker_logger.js
@@ -0,0 +1,24 @@
+/*
+Copyright: Ambrosus Inc.
+Email: tech@ambrosus.com
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
+*/
+
+export default class WorkerLogger {
+  constructor(logger, workerLogRepository) {
+    this.logger = logger;
+    this.workerLogRepository = workerLogRepository;
+  }
+
+  async addLog(message, additionalFields, stacktrace) {
+    const log = {
+      message,
+      ...additionalFields
+    };
+    this.logger.info({...log, stacktrace});
+    await this.workerLogRepository.storeLog({timestamp: new Date(), ...log});
+  }
+}
