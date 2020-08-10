91a5db7fc0c8ee578c192ac59e98e0e66ce92a10
Sergiy Chayka <schayka@gmail.com>
2020-08-10 04:45:36 -0400

    linter fixes

--- src/services/entity_builder.js
+++ src/services/entity_builder.js
@@ -128,11 +128,13 @@ export default class EntityBuilder {
   }
 
   prepareEventForBundlePublication(event) {
-    /*if (event.content.idData.accessLevel === 0) {
+    /*
+    if (event.content.idData.accessLevel === 0) {
       return event;
     }
-    return pick(event, 'content.data');*/
-    
+    return pick(event, 'content.data');
+    */
+
     return event;
   }
 
