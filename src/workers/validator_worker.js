019e4693347343c38add9ff1190de4240880d439
val <vayne.powler@gmail.com>
2021-11-03 10:49:43 +0200

    remove unnecessary line

--- src/workers/validator_worker.js
+++ src/workers/validator_worker.js
@@ -20,7 +20,6 @@ export default class HermesBundlesValidatorWorker extends PeriodicWorker {
     this.bundleRepository = bundleRepository;
     this.bundleStoreWrapper = bundleStoreWrapper;
     this.shelteringWrapper = shelteringWrapper;
-    this.shelteringContract = null;
   }
 
   async periodicWork() {
